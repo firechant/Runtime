02ee17812ee192f0583ff65a4512266e3c3b5a4c
Sarah Scheffler <nerdling@zoho.com>
2014-02-13 21:14:39 -0800

    Initial Commit

---
+++ Runtime/Runtime/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Runtime
+//
+//  Created by Sarah Scheffler on 2/13/14.
+//  Copyright (c) 2014 HMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
